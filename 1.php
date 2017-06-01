f7a70c299abfc19294533832160651ccd9f0e2f8
yangjizhou <1987028242@qq.com>
2017-06-01 14:43:48 +0800

    基本语法练习

---
+++ 1.php
@@ -0,0 +1,25 @@
+<?php
+
+// for ($c = 'a'; $c <= 'z'; $c++) { 
+//   echo $c . "<br />"; 
+// }
+
+for ($i = ord('a'); $i <= ord('z'); $i++) { 
+  echo chr($i) . "\n"; 
+}
+echo '<br>';
+$letters = range('a', 'z'); 
+for ($i = 0; $i < count($letters); $i++) { 
+  echo $letters[$i] . "\n"; 
+}
+
+$array = []; 
+var_dump(empty($array));    // outputs bool(true) 
+$array = new ArrayObject(); 
+var_dump(empty($array));    // outputs bool(false) 
+
+
+$array = []; 
+var_dump(count($array));    // outputs int(0) 
+$array = new ArrayObject(); 
+var_dump(count($array));    // outputs int(0)
